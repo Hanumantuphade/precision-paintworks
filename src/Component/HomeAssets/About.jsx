0e09506da4db867a86cd2b1764b93bec0a9c853b
Hanumantuphade <hanumantuphade63@gmail.com>
2025-06-18 11:32:26 +0530

    done

--- src/Component/HomeAssets/About.jsx
+++ src/Component/HomeAssets/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import SectionTitle from '../../ui/SectionTitle';
+import SectionTitle from '../ui/SectionTitle';
 
 
 const fadeIn = (direction = 'up', delay = 0) => ({
